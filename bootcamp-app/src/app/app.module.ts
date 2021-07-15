1d5ce3982432ae4d6ddf1f5e886925917aa7e22c
evert.devos <evert.devos@euri.com>
2021-07-15 13:18:02 +0200

    added albums folder

--- bootcamp-app/src/app/app.module.ts
+++ bootcamp-app/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
-import { AlbumComponent } from './album/album.component';
+import { AlbumComponent } from './albums/components';
 
 @NgModule({
   declarations: [
